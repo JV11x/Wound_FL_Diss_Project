cf7f2901a715cd9a8d05bab746eaea4afb80a6ae
Jack Vernon <jackvernon94@gmail.com>
2023-10-02 11:38:13 +0100

    Script to test basic Implementation of FL

---
+++ Test_Code/dataset.py
@@ -0,0 +1,33 @@
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+import numpy as np
+
+
+class CarvanaDataset(Dataset):
+    def __init__(self, image_dir, mask_dir, transform=None, subset_size= 20):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+        self.images = [img for img in os.listdir(image_dir) if img.endswith(".png")]
+
+        # Use only the first 'subset_size' images
+        self.images = self.images[:subset_size]
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.image_dir, self.images[index])
+        mask_path = os.path.join(self.mask_dir, self.images[index])
+        image = np.array(Image.open(img_path).convert("RGB"))
+        mask = np.array(Image.open(mask_path).convert("L"), dtype=np.float32)
+        mask[mask == 255.0] = 1.0
+
+        if self.transform is not None:
+            augmentations = self.transform(image=image, mask=mask)
+            image = augmentations["image"]
+            mask = augmentations["mask"]
+
+        return image, mask
+
